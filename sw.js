8f655f9876ca65b4bd2c7a96af3d14c6bc671c08
Vince Allen Cristal <vinceallencristal@gmail.com>
2025-03-19 20:01:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,13 @@ const ASSETS = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS))
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS);
+      })
+      .catch((err) => {
+        console.error('Failed to cache assets:', err);
+      })
   );
 });
 
